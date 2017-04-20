2c9749051dd665d85394301c5e2ea3a9e9bd9906
Szymon_Banas <szymon_banas@epam.com>
2017-04-20 18:25:12 +0200

    login from api

--- src/app/entities/user.ts
+++ src/app/entities/user.ts
@@ -4,6 +4,7 @@ interface UserItem {
 	surname: string;
 	login: string;
 	password: string;
+	token: string;
 	active: boolean
 }
 
@@ -13,14 +14,16 @@ class User implements UserItem {
 	public surname: string;
 	public login: string;
 	public password: string;
+	public token: string;
 	public active: boolean;
 
-	constructor(id: number, name: string, surname: string, login: string, password: string, active: boolean) {
+	constructor(id: number, name: string, surname: string, login: string, password: string, token: string, active: boolean) {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
 		this.login = login;
 		this.password = password;
+		this.token = token;
 		this.active = active;
 	}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Request, RequestOptions, Headers, URLSearchParams, RequestMethod } from '@angular/http';
 import { Observable, ReplaySubject } from 'rxjs';
 import { User } from '../entities';
 import { Router, RouterModule } from '@angular/router';
+import { apiUrl } from './../data/config';
 
 @Injectable()
 export class AuthService {
     private user: User;
     private token: string;
+    private url: string;
     public auth: ReplaySubject<any>;
-    constructor(private router: Router) {
-        this.user = new User(1, 'Szymon', 'Banas', 'simon', 'test', true);
+    constructor(private router: Router, private http: Http) {
+        this.user = new User(0, null, null, null, null, null, true);
         this.token = Math.random().toString(36);
         this.auth = new ReplaySubject(25, 1000);
+        this.url = apiUrl;
     }
 
     public login(login: string, password: string) {
-        if (login === this.user.login && password === this.user.password) {
-            console.log('logged in: ', login);
-            localStorage.setItem('login', login);
-            localStorage.setItem('token', this.token);
-            this.auth.next('login');
+        return this.http.get(this.url+'/users')
+            .map(res => {
+                return res.json().map(user => new User(
+                    user.id,
+                    user.name.first,
+                    user.name.last,
+                    user.login,
+                    user.password,
+                    user.fakeToken,
+                    true
+                ));
+            })
+            .subscribe(res => {
+                res.forEach(user => {
+                    if (login === user.login && password === user.password) {
+                        console.log('logged in: ', login);
+                        localStorage.setItem('login', login);
+                        localStorage.setItem('token', user.token);
+                        this.auth.next('login');
 
-            this.router.navigate(['courses']);
-        }
+                        this.router.navigate(['courses']);
+                    }
+                })
+            });
+
     }
 
     public logout() {
