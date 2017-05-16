0bbb22a7f388248db3f5a77d7f213b53c7f28b3e
Szymon Banaś <szymon_banas@epam.com>
2017-05-17 01:23:41 +0200

    custom date wip

--- src/app/controls/inputdate/inputdate.component.ts
+++ src/app/controls/inputdate/inputdate.component.ts
@@ -21,7 +21,7 @@ const CUSTOM_INPUT_CONTROL_VALIDATOR = {
                     [placeholder]="acceptedFormat"
     				class="form-control input-date"
     				(blur)="onChange($event)">
-            <span class="status-message error-message" *ngIf="control?.errors?.dateFormatError">Invalid format</span>
+            <span class="status-message error-message" *ngIf="dateError">Invalid format</span>
     </div>`,
 	styleUrls: ['./inputdate.component.scss'],
 	providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR, CUSTOM_INPUT_CONTROL_VALIDATOR]
@@ -29,7 +29,7 @@ const CUSTOM_INPUT_CONTROL_VALIDATOR = {
 export class InputDateComponent implements ControlValueAccessor, Validator {
      @Input() public control: FormControl;
     private viewValue: string = '';
-    public modelValue: string;
+    private modelValue: string;
     private acceptedFormat: '00/00/0000'
     private _onChange: Function;
     private _onTouched: Function;
@@ -37,8 +37,9 @@ export class InputDateComponent implements ControlValueAccessor, Validator {
     private dateError: boolean;
 
     public validate(c: FormControl) {
-        let DATE_REGEXP = new RegExp('^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$');
+        const DATE_REGEXP = new RegExp(/^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/);
         console.log('validate', c.value, DATE_REGEXP.test(c.value));
+        this.dateError = true;
         return (!this.dateError && DATE_REGEXP.test(c.value)) ? null : { dateFormatError: {valid: false} };
     }
 
@@ -63,8 +64,9 @@ export class InputDateComponent implements ControlValueAccessor, Validator {
     }
 
     private onChange(event) {
-        console.log('onChange', event.target.value);
+        console.log('dateError', this.dateError);
         this.modelValue = event.target.value;
+        this.dateError = false;
         this.propagateChange(this.modelValue);
     }
 }
